6aea6fa483eee2edeea23921e841b76eda549205
xiefeieric <xiefeieric@gmail.com>
2015-12-02 09:19:11 +0000

    Initial commit

---
+++ app/src/main/java/uk/me/feixie/shoppinglist/activity/AddEditActivity.java
@@ -0,0 +1,69 @@
+package uk.me.feixie.shoppinglist.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import uk.me.feixie.shoppinglist.R;
+
+public class AddEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_edit);
+        initView();
+    }
+
+    private void initView() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_add_edit);
+        setSupportActionBar(toolbar);
+        ActionBar supportActionBar = getSupportActionBar();
+        supportActionBar.setTitle("");
+        //find and show custom back home button
+        supportActionBar.setHomeAsUpIndicator(R.drawable.ic_done_white_24dp);
+        supportActionBar.setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_add_edit, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.action_text) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setView(R.layout.item_alert_dialog);
+            builder.show();
+            return true;
+        }
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+//        if (mToggle.onOptionsItemSelected(item)) {
+//            return true;
+//        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
---
+++ app/src/main/java/uk/me/feixie/shoppinglist/activity/MainActivity.java
@@ -0,0 +1,86 @@
+package uk.me.feixie.shoppinglist.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import uk.me.feixie.shoppinglist.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout dlMain;
+    private ActionBarDrawerToggle mToggle;
+    private Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initToolbar();
+        initViews();
+        initFABtn();
+    }
+
+    private void initToolbar() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+    }
+
+    private void initViews() {
+        //init drawer layout
+        dlMain = (DrawerLayout) findViewById(R.id.dlMain);
+        //init actionbar toggle button
+        mToggle = new ActionBarDrawerToggle(this,dlMain,mToolbar,R.string.DrawerOpen,R.string.DrawerClose);
+        //set drawer layout listener for toggle button
+        dlMain.setDrawerListener(mToggle);
+        //link toggle btton with drawer layout
+        mToggle.syncState();
+    }
+
+    private void initFABtn() {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setRippleColor(getResources().getColor(R.color.lblue));
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                startActivity(new Intent(MainActivity.this, AddEditActivity.class));
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+//        if (mToggle.onOptionsItemSelected(item)) {
+//            return true;
+//        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
