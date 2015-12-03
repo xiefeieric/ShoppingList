6db2c4afebc22863e491cff8bd54f940b095911d
xiefeieric <xiefeieric@gmail.com>
2015-12-03 22:27:44 +0000

    item recyclerview added

--- app/src/main/java/uk/me/feixie/shoppinglist/activity/AddEditActivity.java
+++ app/src/main/java/uk/me/feixie/shoppinglist/activity/AddEditActivity.java
@@ -1,7 +1,13 @@
 package uk.me.feixie.shoppinglist.activity;
 
 import android.app.Dialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.provider.Contacts;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputLayout;
@@ -9,6 +15,8 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDialog;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextUtils;
@@ -20,10 +28,20 @@ import android.view.ViewGroup;
 import android.view.Window;
 import android.view.animation.AnimationUtils;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import uk.me.feixie.shoppinglist.R;
+import uk.me.feixie.shoppinglist.model.Item;
+import uk.me.feixie.shoppinglist.utils.Constants;
 import uk.me.feixie.shoppinglist.utils.UIUtils;
 
 public class AddEditActivity extends AppCompatActivity {
@@ -31,18 +49,27 @@ public class AddEditActivity extends AppCompatActivity {
     private Dialog mDialog;
     private int id;
     private TextInputLayout tiName;
-    private TextInputLayout tiCategory;
     private TextInputLayout tiPrice;
     private TextInputLayout barcode;
     private Spinner spCategory;
-    private String[] mCategory;
+    private RecyclerView rvAddEdit;
+    private List<Item> mItemList;
+    private MyRVAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_edit);
+        mItemList = new ArrayList<>();
         initToolbar();
         initView();
+        registerReceiver(mBroadcastReceiver,new IntentFilter("uk.me.feixie.addIntent"));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mBroadcastReceiver);
     }
 
     private void initToolbar() {
@@ -56,7 +83,10 @@ public class AddEditActivity extends AppCompatActivity {
     }
 
     private void initView() {
-
+        rvAddEdit = (RecyclerView) findViewById(R.id.rvAddEdit);
+        rvAddEdit.setLayoutManager(new LinearLayoutManager(this));
+        mAdapter = new MyRVAdapter();
+        rvAddEdit.setAdapter(mAdapter);
     }
 
     @Override
@@ -76,15 +106,18 @@ public class AddEditActivity extends AppCompatActivity {
         if (id == R.id.action_text) {
             mDialog = new AppCompatDialog(this);
             mDialog.setContentView(R.layout.item_alert_dialog);
+
             tiName = (TextInputLayout) mDialog.findViewById(R.id.tiName);
+
             tiPrice = (TextInputLayout) mDialog.findViewById(R.id.tiPrice);
-            spCategory = (Spinner) mDialog.findViewById(R.id.spCategory);
-            mCategory = new String[]{"Default","Baby", "Baking", "Beverages","Bread/Bakery","Canned Goods","Cereal/Breakfast","Condiments",
-                    "Dairy","Deli","Frozen Foods","Fruits/Vegetables","Meats","Miscellaneous","Non-Food Items","Pasta/Rice","Snacks","Spices"};
-            SpinnerAdapter adapter = new ArrayAdapter<>(this,android.R.layout.simple_dropdown_item_1line, mCategory);
-            spCategory.setAdapter(adapter);
+
             checkItemName();
             if (tiName.getEditText()!=null) {
+
+                ArrayAdapter autoAdapter = new ArrayAdapter(this,android.R.layout.simple_dropdown_item_1line, Constants.ITEMS);
+                AutoCompleteTextView text = (AutoCompleteTextView) tiName.getEditText();
+                text.setAdapter(autoAdapter);
+
                 tiName.getEditText().addTextChangedListener(new TextWatcher() {
                     @Override
                     public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -101,6 +134,12 @@ public class AddEditActivity extends AppCompatActivity {
                     }
                 });
             }
+
+            spCategory = (Spinner) mDialog.findViewById(R.id.spCategory);
+            SpinnerAdapter adapter = new ArrayAdapter<>(this,android.R.layout.simple_dropdown_item_1line, Constants.Category);
+            spCategory.setAdapter(adapter);
+
+
 
             ((ViewGroup) mDialog.getWindow().getDecorView())
                     .getChildAt(0).startAnimation(AnimationUtils.loadAnimation(
@@ -136,8 +175,82 @@ public class AddEditActivity extends AppCompatActivity {
     }
 
     public void dialogOk(View view) {
-        UIUtils.showToast(this,"ok");
+        Item mItem = new Item();
+        String name = tiName.getEditText().getText().toString();
+        mItem.setName(name);
+        String category = spCategory.getSelectedItem().toString();
+        mItem.setCategory(category);
+        String price = tiPrice.getEditText().getText().toString();
+        if (!TextUtils.isEmpty(price)) {
+            mItem.setPrice(price);
+        }
+        Intent intent = new Intent("uk.me.feixie.addIntent");
+        intent.putExtra("item",mItem);
+        sendBroadcast(intent);
         mDialog.dismiss();
+    }
+
+
+    private BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            if (intent.getAction().equalsIgnoreCase("uk.me.feixie.addIntent")) {
+                Item item = (Item) intent.getSerializableExtra("item");
+                mItemList.add(item);
+                mAdapter.notifyItemInserted(0);
+                rvAddEdit.scrollToPosition(0);
+            }
+
+        }
+    };
+
+
+    class MyRVAdapter extends RecyclerView.Adapter<MyViewHolder> {
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View itemView = View.inflate(parent.getContext(), R.layout.item_addedit_rv,null);
+            MyViewHolder myViewHolder = new MyViewHolder(itemView);
+            return myViewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+            Item item = mItemList.get(mItemList.size()-1-position);
+            String name = item.getName();
+            holder.tvName.setText(name);
+            String price = item.getPrice();
+            if (!TextUtils.isEmpty(price)) {
+                holder.tvPrice.setText(price);
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return mItemList.size();
+        }
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView tvName,tvPrice;
+        public RelativeLayout rlItemRv;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            tvName = (TextView) itemView.findViewById(R.id.tvName);
+            tvPrice = (TextView) itemView.findViewById(R.id.tvPrice);
+            rlItemRv = (RelativeLayout) itemView.findViewById(R.id.rl_item_rv);
+
+            tvName.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    UIUtils.showToast(v.getContext(), "Clicked: "+getAdapterPosition());
+                    tvName.setLines(1);
+                }
+            });
+        }
     }
 
 }
---
+++ app/src/main/java/uk/me/feixie/shoppinglist/utils/Constants.java
@@ -0,0 +1,27 @@
+package uk.me.feixie.shoppinglist.utils;
+
+/**
+ * Created by Fei on 03/12/2015.
+ */
+public class Constants {
+
+    public static final String[] ITEMS = new String[] {
+            "diapers", "baby food", "flour", "sugar", "cake mixes", "icings", "cooking oil", "tea", "coffee", "soda", "juice",
+            "Kool-Aid", "hot chocolate", "water", "rolls", "bread", "cakes", "pies", "muffins", "bagels", "doughnuts", "freshly baked cookies",
+            "green beans", "peas", "mushrooms", "beans", "applesauce", "pears", "peaches", "tomato soup", "chicken soup", "broths",
+            "beef soups", "stews", "tomato sauce", "spaghetti sauce", "tuna", "olives", "cereals", "oatmeal", "breakfast bars",
+            "ketchup", "mustard", "pickle relish", "salad dressing", "croutons", "jelly", "milk", "eggs", "prepackaged luncheon meat",
+            "cheeses", "potato salad", "ice cream", "vegetables", "fruit", "pie crusts/pies", "cool whip", "frozen dinners", "dinner supplies",
+            "bagels", "waffles","beef", "chicken","fish","shrimp", "sausage","ham", "pork", "veil", "pre-packaged luncheon meat", "gift cards/wrap", "batteries",
+            "toilet paper", "paper towels", "tissues", "paper plates/cups", "laundry detergent", "dishwashing soap", "shampoo", "toothpaste",
+            "plastic wrap/bags", "aluminium foil", "noodles", "rice", "stuffing", "macaroni","apples", "oranges", "bananas", "grapes", "pomegranates",
+            "celery", "salad", "green beans", "mushrooms", "pumpkin", "Chips", "Crackers", "cinnamon", "basil", "red pepper", "cumin",
+            "multi-purpose seasoning", "taco mixes"
+    };
+
+    public static final String[] Category = new String[] {
+            "Default","Baby", "Baking", "Beverages","Bread/Bakery","Canned Goods","Cereal/Breakfast","Condiments",
+            "Dairy","Deli","Frozen Foods","Fruits/Vegetables","Meats","Miscellaneous","Non-Food Items","Pasta/Rice","Snacks","Spices"
+    };
+
+}
